2b3b3dc238e08fdae7ff96edf7cde5c752714157
shengdaozm <77422229+shengdaozm@users.noreply.github.com>
2022-05-10 20:50:50 +0800

    初始界面

---
+++ src/Algorithm/Kruskal.java
@@ -0,0 +1,10 @@
+package Algorithm;
+
+/**
+ * @Author: .
+ * @Description:
+ * @Date 2022-05-10-20:48
+ * @Modified By:
+ */
+public class Kruskal {
+}
---
+++ src/Algorithm/algorithm.java
@@ -0,0 +1,10 @@
+package Algorithm;
+
+/**
+ * @Author: .
+ * @Description:
+ * @Date 2022-05-10-20:49
+ * @Modified By:
+ */
+public class algorithm {
+}
